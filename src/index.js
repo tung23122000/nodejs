b2dc4ef608cb2fd0fead43ef92b8e26d0f0c06d3
Tung Nguyen <tungnv@dts.com.vn>
2022-07-07 21:17:31 +0700

    vvvv

--- src/index.js
+++ src/index.js
@@ -24,7 +24,7 @@ app.use(express.json()) // xứ lý các trường hơp lưu trữ của file xm
 //template engin
 app.set('view engine', 'hbs'); // sử dụng hbs dể view app
 app.engine('hbs', hbs.engine); // gọi đến thu vien hbs HBS.engine và gán vào hbs
-app.set('views', path.join(__dirname, 'resources\\views')); // địa chỉ lấy ra theo cấu trúc truy cập (mọi thứ từ views)(layout/partials/ home)
+app.set('views', path.join(__dirname, 'resources','views')); // địa chỉ lấy ra theo cấu trúc truy cập (mọi thứ từ views)(layout/partials/ home)
 
 // console.log('PATH:',path.join(__dirname,"resources\\views"));
 
@@ -32,6 +32,6 @@ app.set('views', path.join(__dirname, 'resources\\views')); // địa chỉ lấ
 route(app);
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(` App listening on port ${port}`)
 });
 
